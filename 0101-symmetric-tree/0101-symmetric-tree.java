133b53ea8591491cd904d87721fd5c6ac903a918
Ayusman panda <128355619+ayusmanpanda@users.noreply.github.com>
2026-08-22 11:06:17 +0530

    Time: 0 ms (100%), Space: 43 MB (98.33%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -14,23 +14,35 @@
  * }
  */
 class Solution {
-    public TreeNode invert(TreeNode root){
-        if(root==null) return root;
-        TreeNode l= root.left;
-        TreeNode r = root.right;
-        root.left=invert(r);
-        root.right= invert(l);
-        return root;
-    }
-    public boolean isSame(TreeNode root,TreeNode temp){
+    //METHOD 1
+    // public TreeNode invert(TreeNode root){
+    //     if(root==null) return root;
+    //     TreeNode l= root.left;
+    //     TreeNode r = root.right;
+    //     root.left=invert(r);
+    //     root.right= invert(l);
+    //     return root;
+    // }
+    // public boolean isSame(TreeNode root,TreeNode temp){
+    //     if(root==null && temp==null) return true;
+    //     if(root==null || temp==null) return false;
+    //     if(root.val!=temp.val) return false;
+    //     return isSame(root.left,temp.left)&& isSame(root.right,temp.right);
+    // }
+    // public boolean isSymmetric(TreeNode root) {
+    //     TreeNode temp = root.left;
+    //     invert(temp);
+    //     return isSame(root.right,temp);
+    // }
+
+    // METHOD 2
+    public boolean mirror(TreeNode root,TreeNode temp){
         if(root==null && temp==null) return true;
         if(root==null || temp==null) return false;
         if(root.val!=temp.val) return false;
-        return isSame(root.left,temp.left)&& isSame(root.right,temp.right);
+        return mirror(root.left,temp.right)&& mirror(root.right,temp.left);
     }
     public boolean isSymmetric(TreeNode root) {
-        TreeNode temp = root.left;
-        invert(temp);
-        return isSame(root.right,temp);
+        return mirror(root.right,root.left);
     }
 }
