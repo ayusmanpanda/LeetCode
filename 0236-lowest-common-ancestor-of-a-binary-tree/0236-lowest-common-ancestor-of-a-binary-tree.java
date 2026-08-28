b8041479c6d8fd567a66bf010aca302e4b14835b
Ayusman panda <128355619+ayusmanpanda@users.noreply.github.com>
2026-08-28 20:53:35 +0530

    Time: 399 ms (5.05%), Space: 70.1 MB (17.08%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,41 +8,20 @@
  * }
  */
 class Solution {
-    // public boolean contains(TreeNode root,TreeNode node){
-    //     if(root==null) return false;
-    //     if(root==node) return true;
-    //     return (contains(root.left,node) || contains(root.right,node));
-    // }
-    // public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-    //     if(p==root || q==root)return root;
-    //     if(p==q) return p;
-    //     boolean leftB = contains(root.left,p); //Check present in left branch or right
-    //     boolean rightB = contains(root.right,q);
-    //     if(leftB && rightB) return root;
-    //     if(leftB == true && rightB == false) return lowestCommonAncestor(root.left,p,q);
-    //     if(!leftB && rightB) return lowestCommonAncestor(root.right,p,q);
-    //     return root;
-    // }
-
-
-    public boolean contains(TreeNode root,TreeNode node){
+    public boolean exist(TreeNode root,TreeNode node){
         if(root==null) return false;
-        if(root==node) return true;
-        return (contains(root.left,node) || contains(root.right,node));
+        if(root==node)return true;
+        return exist(root.left,node)||exist(root.right,node);
     }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) return null;
-        if(root==null || p==root || q==root) return root;
-        if(p==q) return p;
-        boolean leftP = contains(root.left,p);
-        boolean leftQ = contains(root.left,q);
-        if (leftP && leftQ){
-            return lowestCommonAncestor(root.left, p, q);
-        }
-        // Both are in right subtree
-        if (!leftP && !leftQ) {
-            return lowestCommonAncestor(root.right, p, q);
-        }
+        if(root==null) return null;
+        if(p==root || q==root) return root;
+        boolean pExistL = exist(root.left,p);
+        boolean qExistL = exist(root.left,q);
+        // if(pExistL && !qExistL) return root;
+        // if(!pExistL && qExistL) return root;
+        if(pExistL && qExistL) return lowestCommonAncestor(root.left, p, q);
+        if(!pExistL && !qExistL) return lowestCommonAncestor(root.right, p, q);
         return root;
     }
 }
