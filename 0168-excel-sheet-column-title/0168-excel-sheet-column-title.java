6e9580e9e6124fcebcbc3b4a2aa3d3c1f7ca23f7
Ayusman panda <128355619+ayusmanpanda@users.noreply.github.com>
2026-09-09 12:39:18 +0530

    Time: 0 ms (100%), Space: 42.8 MB (8.29%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String convertToTitle(int n) {
+        StringBuilder ans =new StringBuilder();
+        while(n>0){
+            n--;
+            ans.append((char)('A'+n%26));
+            n/=26;
+        }
+        return ans.reverse().toString();
+    }
+}
